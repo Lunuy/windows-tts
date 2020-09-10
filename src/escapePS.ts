e4970156f12bf58b9933d18c93e635244454c483
Lunuy <luminuy@gmail.com>
2020-09-10 18:04:13 +0900

    escape

--- src/escapePS.ts
+++ src/escapePS.ts
@@ -2,7 +2,8 @@
 function escapePS(string : string) {
     return string
         .replace(/`/g, "``")
-        .replace(/"/g, "`\"");
+        .replace(/"/g, "`\"")
+        .replace(/\n/g, "`n");
 }
 
 export default escapePS;
